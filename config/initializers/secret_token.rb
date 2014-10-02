e680fb18e28162e3491fb878d7c8f4e35e7401b8
Drew Boardman <drew.e.boardman@gmail.com>
2014-10-02 14:34:16 -0400

    First commit of functioning Rails Blogger app for Web 101 Odin Project section.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '3328e18e03dc7ca4900fd7cf12fb1a2837366d618c101027ce618a82c12c4957cfc3613cc7593dc5d37c84c620acbe53c9e91d69b312baa8ef0256ccef7fbbe7'
